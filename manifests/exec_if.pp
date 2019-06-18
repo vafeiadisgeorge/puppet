5934fc91d53fdeb113447b64aec1df82abfe5afc
Vafeiadis George <george.vafeiadis@wirecard.com>
2019-06-18 16:13:21 +0300

    hello

--- manifests/exec_if.pp
+++ manifests/exec_if.pp
@@ -1,7 +1,7 @@
 exec { 'test if files exist and execute':
 command => 'cat /etc/passwd',
-creates => /tmp/passwd
-user => 'root'
+creates => '/tmp/passwd',
+user => 'root',
 onlyif => '/bin/ls /tmp/test/*',
 logoutput => true,
 }
